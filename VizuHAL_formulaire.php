dfec9b059d8e66d488a0bf861a3ef39711f5f864
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2021-09-09 15:52:15 +0200

    mdi-help

--- VizuHAL_formulaire.php
+++ VizuHAL_formulaire.php
@@ -9,7 +9,7 @@
                                                         <div class="input-group-prepend">
                                                             <button type="button" tabindex="0" class="btn btn-info" data-html="true" data-toggle="popover" data-trigger="focus" title="" data-content='Code visible dans l&apos;URL d&apos;une collection.
                                             Exemple IPR-MOL est le code de la collection http://hal.archives-ouvertes.fr/ <span class="font-weight-bold">IPR-PMOL</span> de l&apos;équipe Physique moléculaire de l&apos;unité IPR UMR CNRS 6251' data-original-title="">
-                                                            <i class="mdi mdi-comment-question text-white"></i>
+                                                            <i class="mdi mdi-help text-white"></i>
                                                             </button>
                                                         </div>
 																												
